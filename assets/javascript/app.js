d8e30d98a7cd6b180bfc30210243a3a577827a77
drichard1989 <drichard1989@icloud.com>
2016-11-30 17:13:59 -0600

    Had to restructure my homework becuase I didnt read the assignment before, like a dummy. Now I cannot get new buttons added to display gifs when they are clicked, even though the button element has the same classes and attributes.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,54 @@
 $(document).ready(function(){
 
+// This is the array of animals that I created.
+var animalArray = ["Elephant", "Ant", "Alligator", "Cat", "Dog", "Bird", "Peacock", "Giraffe", "Lion", "Seal", "Whale", "Shark", "Bee"]
+
 // Creating an empty global variable so I can call on this global variable inside both event listeners. 
 var animal ;
+
+// This variable is going to be set to the button in the for loop that is going to be dynamically added when the page loads. 
+var animalPreButton ;
+
+// Function that is written to be executed when the page loads. 
+function pageAnimalLoad() {
+	// Goes through the animalArray and for each item, creates a button with the attributes listed below. 
+	for (i=0; i<animalArray.length; i++) {
+		animalPreButton = $("<button>").addClass("btn btn-secondary animalButton").attr("type", "button").text(animalArray[i]).attr("data-animal", animalArray[i]);
+		// Appends those buttons inside of the section in the HTML document of #animalButtons.
+		$("#animalButtons").append(animalPreButton);
+		console.log(animalArray[i]);
+	}
+};
+
+// This function is called when the submit button to add an animal is clicked and there are characters in the text box.
+function addAnimal() {
+
+		// Clears the #animalButtons container in the HTML document.
+		$("#animalButtons").empty();
+		// Makes the value of animal equal to the text that was inserted in the search box without whitespace.
+		animal = $("#animalSearchText").val().trim();
+		// Adds the animal that was searched into the array.
+		animalArray.push(animal);
+		// Consoles the array. 
+		console.log(animalArray);
+		// This line creates a local variable named button that has classes with bootstrap in mind, adds the animals name as the text of the button, and adds an attribute of data-animal with a value of the animals name. 
+		var button = $("<button>").addClass("btn btn-secondary animalButton").attr("type", "button").text(animal).attr("data-animal", animal);
+		// Consoling...
+		console.log(button);
+		// And at the end, setting the value of the search box to nothing so that the text in the search box dissapears when the search is run.
+		$("#animalSearchText").val("");
+
+		for (i=0; i<animalArray.length; i++) {
+		animalPreButton = $("<button>").addClass("btn btn-secondary animalButton").attr("type", "button").text(animalArray[i]).attr("data-animal", animalArray[i]);
+		$("#animalButtons").append(animalPreButton);
+		console.log(animalArray[i]);
+		};
+};
+
+
+// When the page loads, it is running the pageAnimalLoad function, which is defined below. 
+pageAnimalLoad();
+
 
 // A function is performed when the #addingAnimals id is clicked on.
 $("#addingAnimals").on("click", function() {
@@ -12,16 +59,10 @@ $("#addingAnimals").on("click", function() {
 	}
 	// Once the box has some content in it, it will allow you to create a button. 
 	else{
+
+		addAnimal();
 		// This line will make a local variable named animal which is equal to the value of the value of the text input trimmed without whitespace
-		animal = $("#animalSearchText").val().trim();
-		// This line creates a local variable named button that has classes with bootstrap in mind, adds the animals name as the text of the button, and adds an attribute of data-animal with a value of the animals name. 
-		var button = $("<button>").addClass("btn btn-secondary").attr("type", "button").text(animal).attr("data-animal", animal);
-		// Here we are appending the button in the HTML with the append method.
-		$("#animalButtons").append(button);
-		// Consoling...
-		console.log(button);
-		// And at the end, setting the value of the search box to nothing so that the text in the search box dissapears when the search is run.
-		$("#animalSearchText").val("");
+		
 	};
 	// Keeps the page from refreshing.
 	return false;
@@ -29,17 +70,18 @@ $("#addingAnimals").on("click", function() {
 
 
 // This is the event listener for a click on one of the animal buttons that were created.
-$("#animalButtons").on("click", function() {
+$(".animalButton").on("click", function() {
 
 	// This emptys out the gifs from the previous return to only have 10 gifs at a time in the bay
 	$("#animalGifs").empty();
 	
+	var animalSearch = $(this).attr("data-animal");
 	// This is the API key for Giphy.
-	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=dc6zaTOxFJmzC&limit=10";
+	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animalSearch + "&api_key=dc6zaTOxFJmzC&limit=10";
 	// Here I am consoling the URL to make sure it is plugged in correctly. 
 	console.log(queryURL);
 	// Consoling the value of animal to make sure it is being stored correctly from the previous event listener. 
-	console.log(animal);
+	console.log(animalSearch);
 
 	// Simple ajax syntax to get the array object from Giphy.
 	$.ajax({
@@ -70,6 +112,7 @@ $("#animalButtons").on("click", function() {
 
                     // Sets the default src of the image of the still image returned from the gif's object 
                     animalImage.attr('src', results[i].images.fixed_height_still.url);
+                    animalImage.attr("data-still", results[i].images.fixed_height_still.url)
                     // Adds an attribute of data-animate equal to the url for the animated gif
                     animalImage.attr('data-animate', results[i].images.fixed_height.url);
                     
